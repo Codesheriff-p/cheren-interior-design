af7f50d4bc2c147f30bc4641ce46535391c9f9f1
Codesheriff-p <princeamaeze04@gmail.com>
2026-07-20 17:18:10 +0100

    updated

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import './i18n'  // just import it, before App
-import App from './App'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import "./i18n"; // just import it, before App
+import App from "./App";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
